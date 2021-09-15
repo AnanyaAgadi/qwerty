13b7b1a10255d770dad1aa99f7c819b5e98cbd78
Ananya <avagadi15@gmail.com>
2021-09-15 18:23:05 +0530

    4.5 Adding featured products dynamically to image carousel

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,5 +1,24 @@
 class SimplePagesController < ApplicationController
+  
+
+  def index
+    @products = Product.limit(3)
+  end
+
+  # GET /products/1 or /products/1.json
+  def show
+  end
+
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+  end
+  
   def landing_page
-    @featured_product = Product.first
+    @products = Product.limit(3)
   end
 end
