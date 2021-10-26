bd86cc5ca482ac91894cc3bc2c104e522c8d88ef
Ananya <avagadi15@gmail.com>
2021-10-26 17:31:29 +0530

    5.2 Http GET and params

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   resources :products 
   resources :orders, only: [:index,:show,:create,:destroy]
-  root 'products#index'
+  # Below is the dummy parameter checking 5.2 params
+  root 'simple_pages#landing_page'
+  # root 'products#index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
