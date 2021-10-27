4662bde08d09c9e29445fac1d76bbec027ad47e2
Ananya <avagadi15@gmail.com>
2021-10-27 16:03:02 +0530

    5.2 forms search integration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,12 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.all    
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.search(search_term)
+    else
+      @products = Product.all
+    end  
   end
 
   # GET /products/1 or /products/1.json
@@ -67,3 +72,30 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:name, :description, :image_url, :price)
     end
 end
+
+# steps on creating a search option that filters out and gives us appropriate results
+# Add the erb for the form using form_with. Mention an attribute for each form field
+# Next add that attribute to a params 
+# Next that will also be the name of the parameter that gets passed to the index action as our search term.
+# We now need to capture our search term and use it to filter our products. 
+# In any controller action where data gets passed in with therequest, we can access any parameters in the request using a hash
+  # called params.
+
+
+# PUT POST PATCH 
+ 
+# According to rails convention,
+
+# PUT is used for updating an existing resource
+
+# POST is used for creating a new resource
+
+# In rails 4, PUT has been changed to PATCH to avoid confusion.
+
+# A request using the POST method should act upon the resource collection; adding a new resource to the collection.Example URL: http://example.com/resources
+
+# A request using the PUT HTTP verb should act upon a single resource within the collection; replacing the resource wholly upon the server Example URL: http://example.com/resource/1
+
+# A request using the PATCH HTTP verb should act upon a single resource within the collection; updating certain attributes upon the resource where it stands Example URL: http://example.com/resource/1
+
+# Rails 4 now makes use of the PATCH verb over the PUT verb for updating a resource.
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
-	
+  # 5.2 adding the search functionality query below
+  def self.search(search_term)
+    Product.where("name LIKE?", "%#{search_term}%")
+  end
 end
