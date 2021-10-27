4934414733b55741de1e69d716d27feb78239cc8
Ananya <avagadi15@gmail.com>
2021-10-27 21:35:17 +0530

    added users and 5.3 email part-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :products 
+  resources :products
+  resources :users 
   resources :orders, only: [:index,:show,:create,:destroy]
   # Below is the dummy parameter checking 5.2 params
   root 'simple_pages#landing_page'
@@ -10,6 +11,8 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/faq'
   get 'simple_pages/login'
+  get 'simple_pages/thank_you'
+  post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_130604) do
+ActiveRecord::Schema.define(version: 2021_10_27_104308) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2021_09_13_130604) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "phone"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
