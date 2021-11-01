0797e905120de59b315baecb3bca511f1a57f03d
Ananya <avagadi15@gmail.com>
2021-11-01 15:47:58 +0530

    added missing view files for users , 5.5 part-2

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, user_id: user.id
+    can :manage, User, id: user.id
   end
     # Define abilities for the passed in user here. For example:
     #
