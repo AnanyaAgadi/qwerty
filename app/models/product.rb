8f673e2487d03a15e2bcf42aa8aa4f28478db92f
Ananya <avagadi15@gmail.com>
2021-11-03 22:56:32 +0530

    fix in comments and addition of products,login pg

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
-class Product < ApplicationRecord
+  class Product < ApplicationRecord
   # 5.2 adding the search functionality query below
+  has_many :comments
+  
   def self.search(search_term)
     Product.where("name LIKE?", "%#{search_term}%")
   end
 
-  has_many :comments
 
   def highest_rating_comment
     comments.rating_desc.first
